f90f321d7f6078c373c23899ac9639441036d835
NanoRonin <rohinthespiderman@gmail.com>
2026-06-24 16:34:20 -0500

    death
    redid death screen and other small bug fixes

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+if os.getenv("LOVE2D_TOOLS") then pcall(require, "_love2d_tools_bridge") end
 require('functions')
 
 function love.load()
@@ -5,16 +6,17 @@ function love.load()
     defaultFont = love.graphics.getFont()
     deadFont = love.graphics.newFont(40)
     love.window.setFullscreen(true)
-    love.window.setTitle("Pranay likes ducks")
+    love.window.setTitle("chicken and the butcher")
     resetGame()
 end
 
 function love.update(dt)
+    delta = dt
     if state == "playing" then
         spawnTimer = spawnTimer + dt
         player.Sprite = love.graphics.newImage('sprites/chick.png')
 
-        if spawnTimer >= 2 then
+        if spawnTimer >= 0.25 then
             addEnemy()
             spawnTimer = 0
         end
@@ -73,26 +75,40 @@ end
 function love.draw()
     love.graphics.draw(background, 0, 0, 0, 5, 5)
     love.graphics.draw(player.sprite, player.x, player.y, 0, 0.2, 0.2)
-    --love.graphics.circle("line", player.x + player.width/2, player.y + player.height/2, 40)
+    love.graphics.circle("line", player.x + player.width/2, player.y + player.height/2, 40)
     
     for i,v in ipairs(enemies) do
         love.graphics.draw(v.sprite, v.x, v.y, 0, 0.2, 0.2)
-        --love.graphics.rectangle("line", v.x , v.y, 100, 100)
+        love.graphics.rectangle("line", v.x , v.y, 100, 100)
     end
 
-    local scoreText = "Assassination attempts survived: " .. score
-    local scoreX = love.graphics.getWidth() / 2 - deadFont:getWidth(scoreText) / 2
-    love.graphics.setFont(deadFont)
-    love.graphics.print(scoreText, scoreX, 10)
-    love.graphics.setFont(defaultFont)
+    if state == "playing" then
+        local scoreText = "Assassination attempts survived: " .. score
+        local scoreX = love.graphics.getWidth() / 2 - deadFont:getWidth(scoreText)/2
+        love.graphics.setFont(deadFont)
+        love.graphics.print(scoreText, scoreX, 10)
+        love.graphics.setFont(defaultFont)
+    end
+    
+
+    local fpsText = "FPS: " .. love.timer.getFPS()
+    local fpsX = love.graphics.getWidth() - defaultFont:getWidth(fpsText) - 10
+    love.graphics.print(fpsText, fpsX, 10)
 
     if state == "dead" then
         player.sprite = love.graphics.newImage('sprites/meatChop.png')
         love.graphics.setFont(deadFont)
-        local text = "pranay is a camel"
+
+        local text = "You survived " .. score .. " assassination attempts"
         local x = love.graphics.getWidth() / 2 - deadFont:getWidth(text) / 2
         local y = love.graphics.getHeight() / 2 - deadFont:getHeight() / 2
         love.graphics.print(text, x, y)
+
+        text = "Space to replay"
+        local x = love.graphics.getWidth() / 2 - deadFont:getWidth(text)/2
+        local y = love.graphics.getHeight() / 2 - deadFont:getHeight()/2
+        love.graphics.print(text, x, y + 100)
+
         love.graphics.setFont(defaultFont)
     end
 end
